8db80e8dd208c7927ca03afcb63677d4118942c6
Prabhat Kumar <pk003105@gmail.com>
2025-02-19 03:28:14 +0530

    initialize Next.js project with authentication and UI components

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// import LoaderPage from "@/components/loader";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click me</Button>
+      {/* <LoaderPage /> */}
+    </div>
+  );
+}
