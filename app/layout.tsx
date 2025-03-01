06682c47c10f9ffdd1423b535ee8a3d10e0fe7b1
Prabhat Kumar <pk993105@gmail.com>
2025-03-01 09:07:58 +0530

    feat: add Open Graph metadata and SVG for improved social sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,54 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import "./globals.css";
+import type { Metadata } from "next";
 
-// Suppress hydration warnings for known mismatches (optional, use with caution)
+const domain = process.env.NEXT_PUBLIC_BASE_URL || "";
+const title = "Portfolio Website";
+const description = "Portfolio Website by Prabhat Kumar";
+export const metadata: Metadata = {
+  title: title,
+  description: description,
+  metadataBase: new URL("https://gemini-ai-mock-interview.vercel.app"),
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: title,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    title: title,
+    description: description,
+    url: domain,
+    type: "website",
+    siteName: title,
+
+    images: [
+      {
+        url: domain + "/opengraph-image.svg",
+        width: 800,
+        height: 800,
+        alt: title,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: title,
+      template: title,
+    },
+    images: [
+      {
+        url: domain + "/opengraph-image.svg",
+        alt: title,
+      },
+    ],
+    description: description,
+  },
+};
 
 export default function RootLayout({
   children,
